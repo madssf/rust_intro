ef4a4f307ed1ae8c809ace378e220ae19ed71c0a
madssf <mads.steenfeldt.foss@gmail.com>
2022-07-01 16:46:04 +0200

    nightly + rocket web server

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,13 @@
-use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn root() -> String {
+    format!("Hello world")
+}
 
 fn main() {
-    println!("Guess the number!");
-
-    let secret_number = rand::thread_rng().gen_range(1..101);
-
-    
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin() 
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess: u32 = match guess.trim().parse() {
-        Ok(num) => num,
-        Err(_) => {
-            println!("The guess must be a number!");
-            continue;
-        },
-    };
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-            
-    }
-
-    
-
+    rocket::ignite().mount("/", routes![root]).launch();
 }
